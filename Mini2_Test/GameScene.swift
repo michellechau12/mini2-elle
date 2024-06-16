a6e72b31628958a70611c2f1175f2517795918e6
Michelle C <micheelechau@gmail.com>
2024-06-17 00:40:09 +0700

    create and give map physics body

--- Mini2_Test/GameScene.swift
+++ Mini2_Test/GameScene.swift
@@ -10,65 +10,11 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var bombSites: [BombSiteModel] = []
-    
     override func didMove(to view: SKView) {
-        //        if let tileMap = childNode(withName: "Maze") as? SKTileMapNode {
-        //            giveMapPhysicsBody(map: tileMap)
-        //        }
-//        giveMapPhysicsBody(map: childNode(withName: "Maze") as! SKTileMapNode)
-        setupPhysics()
-        setupBombSites()
-        setupPlayer()
-        
-    }
-    
-    func setupPhysics() {
-        //contact delegate:
-        
-        //fbi node physics body:
-        
-        //terrorist node physics body:
-        
-        //map physics body
-        let map = childNode(withName: "Maze") as! SKTileMapNode
-        
-        let tileMap = map // the tile map to be given physics body
-        let tileSize = tileMap.tileSize // the size of each tile map
-        let halfWidth = CGFloat(tileMap.numberOfColumns) / 2.0 * tileSize.width // half width of tile map
-        let halfHeight = CGFloat(tileMap.numberOfRows) / 2.0 * tileSize.height // half height of tile map
-        
-        for col in 0..<tileMap.numberOfColumns {
-            for row in 0..<tileMap.numberOfRows {
-                
-                if let tileDefinition = tileMap.tileDefinition(atColumn: col, row: row) {
-                    
-                    let isEdgeTile = tileDefinition.userData?["AddBody"] as? Int
-                    if isEdgeTile == 1 {
-                        let tileArray = tileDefinition.textures //get the tile textures in array
-                        let tileTexture = tileArray[0] //get the first texture
-                        let x = CGFloat(col) * tileSize.width - halfWidth + (tileSize.width/2)
-                        let y = CGFloat(row) * tileSize.height - halfHeight + (tileSize.height/2)
-                        let tileNode = SKNode()
-                        
-                        tileNode.position = CGPoint(x: x, y: y)
-                        tileNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: (tileTexture.size().width), height: tileTexture.size().height))
-                        tileNode.physicsBody?.affectedByGravity = false
-                        tileNode.physicsBody?.allowsRotation = false
-                        tileNode.physicsBody?.restitution = 0
-                        tileNode.physicsBody?.isDynamic = false
-                        tileNode.physicsBody?.friction = 20.0
-                        tileNode.physicsBody?.mass = 30.0
-                        tileNode.physicsBody?.contactTestBitMask = 0
-                        //                        tileNode.physicsBody?.fieldBitMask = 0
-                        //                        tileNode.physicsBody?.collisionBitMask = 0
-                        
-                        tileMap.addChild(tileNode)
-                    }
-                }
-            }
+        if let tileMap = childNode(withName: "Maze") as? SKTileMapNode {
+            giveMapPhysicsBody(map: tileMap)
         }
-        
+        setupPlayer()
     }
     
     //Add physic body to map
@@ -106,6 +52,7 @@ class GameScene: SKScene {
                         
                         tileMap.addChild(tileNode)
                     }
+                    
                 }
             }
         }
@@ -125,31 +72,5 @@ class GameScene: SKScene {
         addChild(playerCatNode)
     }
     
-    // Setup bombsite
-    func setupBombSites() {
-        for child in self.children {
-            if child.name == "BombSite" {
-                if let child = child as? SKSpriteNode {
-                    let bombSitePosition = child.position
-                    let bombSiteSize = child.size
-                    let bombSite = BombSiteModel(bombSitePosition: bombSitePosition, bombSiteSize: bombSiteSize)
-                    
-                    bombSites.append(bombSite)
-                    //Tes print:
-                    print("bombsite position is: \(bombSitePosition) and size is: \(bombSiteSize)")
-                }
-            }
-        }
-    }
-    
 }
 
-
-
-//Setup Map
-//    func setupMap() {
-//        if let tileMap = childNode(withName: "Maze") as? SKTileMapNode {
-//            giveMapPhysicsBody(map: tileMap)
-//        }
-//        plantBomb()
-//    }
